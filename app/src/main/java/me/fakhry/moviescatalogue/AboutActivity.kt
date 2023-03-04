9ff608b6d6b47b39eced611f8a0dcda549aa86a5
Fakhry <fakhry.linux@gmail.com>
2023-03-04 13:35:44 +0800

    Add About page

---
+++ app/src/main/java/me/fakhry/moviescatalogue/AboutActivity.kt
@@ -0,0 +1,20 @@
+package me.fakhry.moviescatalogue
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import coil.load
+import me.fakhry.moviescatalogue.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAboutBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivPhoto.load(getString(R.string.photo_url))
+        binding.tvName.text = getString(R.string.my_name)
+        binding.tvEmail.text = getString(R.string.my_email)
+    }
+}
--- app/src/main/java/me/fakhry/moviescatalogue/MainActivity.kt
+++ app/src/main/java/me/fakhry/moviescatalogue/MainActivity.kt
@@ -2,6 +2,8 @@ package me.fakhry.moviescatalogue
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import me.fakhry.moviescatalogue.databinding.ActivityMainBinding
@@ -18,6 +20,20 @@ class MainActivity : AppCompatActivity() {
 
         list.addAll(getListMovies())
         showRecyclerList()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_about -> {
+                startActivity(Intent(this@MainActivity, AboutActivity::class.java))
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun getListMovies(): ArrayList<Movie> {
